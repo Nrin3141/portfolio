09ead4c4201ae5797018061c6aa69fcfa6fbfbc0
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-05-14 12:04:24 +0200

    Add dynamic header to frontpage

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
