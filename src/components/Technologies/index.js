3b90178df64d369a3947fbcef8ac86068319971e
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-05-14 19:52:33 +0200

    Add Coding page and components

---
+++ src/components/Technologies/index.js
@@ -0,0 +1,149 @@
+import React from "react"
+import Paper from "@material-ui/core/Paper"
+import PropTypes from "prop-types"
+import classNames from "classnames"
+import { withStyles } from "@material-ui/core/styles"
+
+const styles = {
+  white: {
+    width: "80%",
+    margin: 10,
+    padding: 20,
+    textAlign: "center",
+  },
+}
+const Technologies = ({ classes }) => (
+  <Paper className={classes.white}>
+    <h2>Technologies I use and love: </h2>
+    <div className="icon-container">
+      <div className="size">
+        <div className="tile small" id="html" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="css" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="js" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="meteor" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="react" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="node" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="graphql" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="mongo" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="bash" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="material" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="next" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="p5" />
+      </div>
+      <div className="size">
+        <div className="tile small" id="atom" />
+      </div>
+    </div>
+
+    <style jsx>{`
+      .icon-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+      }
+      .tile {
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+
+      .size {
+        height: 10vw;
+        width: 10vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .small {
+        width: 8vw;
+        height: 8vw;
+      }
+      .small:hover {
+        width: 10vw;
+        height: 10vw;
+      }
+      @media only screen and (max-width: 800px) {
+        .size {
+          height: 22vw;
+          width: 22vw;
+        }
+        .small {
+          width: 15vw;
+          height: 15vw;
+        }
+        .small:hover {
+          width: 22vw;
+          height: 22vw;
+        }
+      }
+      #material {
+        background-image: url("/static/images/material.png");
+      }
+      #atom {
+        background-image: url("/static/images/atom.png");
+      }
+      #html {
+        background-image: url("/static/images/html5.svg");
+      }
+      #bash {
+        background-image: url("/static/images/bash.png");
+      }
+      #p5 {
+        background-image: url("/static/images/p5js.png");
+      }
+      #next {
+        background-image: url("/static/images/nextjs.png");
+      }
+      #css {
+        background-image: url("/static/images/css3.svg");
+      }
+      #js {
+        background-image: url("/static/images/javascript.png");
+      }
+      #meteor {
+        background-image: url("/static/images/meteor-text.svg");
+      }
+      #mongo {
+        background-image: url("/static/images/mongo.svg");
+      }
+      #graphql {
+        background-image: url("/static/images/graphql.svg");
+      }
+      #node {
+        background-image: url("/static/images/node.svg");
+      }
+      #react {
+        background-image: url("/static/images/react.svg");
+      }
+    `}</style>
+  </Paper>
+)
+Technologies.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Technologies)
---
+++ src/pages/coding.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Developer from "../components/Developer"
+
+const Coding = () => (
+  <Layout>
+    <style jsx>{`
+      div {
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+      }
+    `}</style>
+  </Layout>
+)
+
+export default Coding
