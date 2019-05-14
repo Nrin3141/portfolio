1723c8c7dfb076e048b34ffb093c951baea916ec
Nrin <Nrin3141@users.noreply.github.com>
2019-05-14 22:01:16 +0200

    Import Technologies section

--- src/components/Developer/index.js
+++ src/components/Developer/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Projects from "../Projects"
 import AboutMe from "../AboutMe"
-//import Technologies from "../Technologies"
+import Technologies from "../Technologies"
 
 const Developer = () => {
   return (
     <div>
       <AboutMe />
       <Projects />
-      {/*<Technologies />*/}
+      <Technologies />
     </div>
   )
 }
