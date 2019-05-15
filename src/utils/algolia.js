4a9ab6ccff0e304128160e508fc6f1fe899a0ecd
Nrin <Nrin3141@users.noreply.github.com>
2019-05-15 15:06:39 +0200

    Style search component

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -1,23 +1,3 @@
-const pageQuery = `{
-  pages: allMarkdownRemark(
-    filter: {
-      fileAbsolutePath: { regex: "/pages/" },
-      frontmatter: {purpose: {eq: "page"}}
-    }
-  ) {
-    edges {
-      node {
-        objectID: id
-        frontmatter {
-          title
-          slug
-        }
-        excerpt(pruneLength: 5000)
-      }
-    }
-  }
-}`
-
 const postQuery = `{
   posts: allMarkdownRemark(
     filter: { fileAbsolutePath: { regex: "/blog/" } }
@@ -45,12 +25,6 @@ const flatten = arr =>
 const settings = { attributesToSnippet: [`excerpt:20`] }
 
 const queries = [
-  {
-    query: pageQuery,
-    transformer: ({ data }) => flatten(data.pages.edges),
-    indexName: `Pages`,
-    settings,
-  },
   {
     query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
