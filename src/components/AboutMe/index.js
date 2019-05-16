78f3f2df022c853d19b7001b0cba81a532fa23ed
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-05-17 00:41:52 +0200

    Move styling into jsx for AboutMe component

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,8 +1,5 @@
 import React from "react"
-import PropTypes from "prop-types"
-import classNames from "classnames"
 import { Link } from "gatsby"
-import { withStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import Paper from "@material-ui/core/Paper"
 import { MuiThemeProvider } from "@material-ui/core/styles"
@@ -10,44 +7,7 @@ import { theme } from "../../utils/getPageContext.js"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-const styles = theme => ({
-  bigAvatar: {
-    width: "30vw",
-    height: "30vw",
-    backgroundImage: `url(${""})`,
-  },
-  width: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-  },
-  col: {
-    flexDirection: "column",
-  },
-  headline: {
-    textAlign: "center",
-  },
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "2vh",
-    padding: "5vh",
-  },
-  responsive: {
-    [theme.breakpoints.up("sm")]: {
-      flexDirection: "row",
-    },
-  },
-})
-
-const AboutMe = props => {
-  const { classes } = props
-
+const AboutMe = () => {
   return (
     <StaticQuery
       query={graphql`
@@ -64,65 +24,89 @@ const AboutMe = props => {
       render={data => {
         return (
           <MuiThemeProvider theme={theme}>
-            <Paper
-              className={classNames(classes.container, classes.responsive)}
-            >
-              <Img
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "50%",
-                  margin: "5vh",
-                }}
-                fluid={data.file.img.fluid}
-              />
-              <div className={classNames(classes.width, classes.col)}>
-                <h2 className={classes.headline}>About Me</h2>
+            <div className="absolute">
+              <Paper className="muipaper row">
+                <Img
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "50%",
+                    margin: "5vh",
+                  }}
+                  fluid={data.file.img.fluid}
+                />
+                <div className="width column">
+                  <h2 className="headline">About Me</h2>
+                  <p>
+                    Hi there, I am Rico - a self-taught full-stack-developer
+                    from Berlin. I like problem solving. I like programming. I
+                    like photography. And I am a passionate traveler.
+                  </p>
+                </div>
+              </Paper>
+              <Paper className="muipaper column">
+                <h2>How can I help you?</h2>
                 <p>
-                  Hi there, I am Rico - a self-taught full-stack-developer from
-                  Berlin. I like problem solving. I like programming. I like
-                  photography. And I am a passionate traveler.
+                  Do you want to have your own website too? Or a Web app? Mobile
+                  game? Online Shop? Blog?! Or do you have an amazing idea but
+                  lack the technical skills to actually do it? Don't hesitate to
+                  ask.
                 </p>
-              </div>
-            </Paper>
-            <Paper className={classes.container}>
-              <h2>How can I help you?</h2>
-              <p>
-                Do you want to have your own website too? Or a Web app? Mobile
-                game? Online Shop? Blog?! Or do you have an amazing idea but
-                lack the technical skills to actually do it? Don't hesitate to
-                ask.
-              </p>
-              <Link to="/contact">
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  style={{ margin: "5vh" }}
-                >
-                  Get in touch
-                </Button>
-              </Link>
-              <p>
-                Or are you in search of a full-stack developer to expand your
-                team? Somebody who can easily learn and integrate new
-                technologies and loves to expand his expertise constantly?
-              </p>
-            </Paper>
-            <style>{`
+                <Link to="/contact">
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={{ margin: "5vh" }}
+                  >
+                    Get in touch
+                  </Button>
+                </Link>
+                <p>
+                  Or are you in search of a full-stack developer to expand your
+                  team? Somebody who can easily learn and integrate new
+                  technologies and loves to expand his expertise constantly?
+                </p>
+              </Paper>
+            </div>
+
+            <style jsx>{`
               p {
                 margin: 0;
                 padding: 0;
               }
-              `}</style>
+              .width {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+                height: 100%;
+              }
+              .column {
+                flex-direction: column;
+              }
+              .headline {
+                text-align: center;
+              }
+              .absolute :global(.muipaper) {
+                display: flex;
+                flex-direction: column;
+                text-align: center;
+                justify-content: center;
+                align-items: center;
+                margin-top: 2vh;
+                padding: 5vh;
+              }
+              @media only screen and (min-width: 600px) {
+                .absolute :global(.row) {
+                  display: flex;
+                  flex-direction: row;
+                }
+              }
+            `}</style>
           </MuiThemeProvider>
         )
       }}
     />
   )
 }
-
-AboutMe.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(AboutMe)
+export default AboutMe
