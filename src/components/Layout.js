43e5d26d30db2114d8fd8e889b98265c1f1a7cdf
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-05-16 21:25:26 +0200

    Add a fix for content flash

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,10 +38,6 @@ const Layout = ({ children, disableMenu, noSpacing }) => (
           }}
         >
           <Helmet>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            />
             <meta
               name="viewport"
               content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
