cd99689673f3cefbca62e035cf93c935df801400
Nrin <Nrin3141@users.noreply.github.com>
2019-05-17 23:12:10 +0200

    Add more breakpoints for responsiveness

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
+import { theme } from "../../utils/getPageContext.js"
 
 const styles = {
   appBar: {
@@ -14,16 +15,39 @@ const styles = {
     padding: "20px",
   },
   toolbar: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "center",
     background: "white",
-    flexWrap: "wrap",
+    [theme.breakpoints.up(750)]: {
+      flexDirection: "row",
+    },
+    [theme.breakpoints.up(1500)]: {
+      justifyContent: "center",
+    },
   },
   Button: {
     color: "white",
     textDecoration: "none",
     textTransform: "capitalize",
+    [theme.breakpoints.up(1500)]: {
+      margin: "0 20px",
+    },
+  },
+  buttonwrapper: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    [theme.breakpoints.down(400)]: {
+      flexDirection: "column",
+      width: "auto",
+    },
+    [theme.breakpoints.up(1500)]: {
+      justifyContent: "center",
+      width: "auto",
+    },
   },
 }
 
@@ -33,48 +57,55 @@ class BottomAppBar extends React.Component {
     return (
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
-          <Button
-            className={classes.Button}
-            style={{ textDecoration: "none" }}
-            href={"https://github.com/Nrin3141"}
-            target="blank"
-            rel="noopener"
-          >
-            Github <i className="fab fa-github" />
-          </Button>
-          <Button
-            className={classes.Button}
-            style={{ textDecoration: "none" }}
-            href="https://www.linkedin.com/in/ricotrebeljahr"
-            target="blank"
-            rel="noopener"
-          >
-            Linked
-            <i className="fab fa-linkedin" style={{ color: "#4875B4" }} />
-          </Button>
-          <Button
-            className={classes.Button}
-            style={{ textDecoration: "none" }}
-            href="https://stackoverflow.com/story/ricotrebeljahr"
-            target="blank"
-            rel="noopener"
-          >
-            <i className="fab fa-stack-overflow" style={{ color: "#F48024" }} />{" "}
-            Stackoverflow
-          </Button>
-          <Button
-            className={classes.Button}
-            style={{ textDecoration: "none" }}
-            href="https://codepen.io/Nrin/"
-            target="blank"
-            rel="noopener"
-          >
-            Codepen{" "}
-            <i
-              className="fab fa-codepen"
-              style={{ color: "rgb(100, 100, 100)" }}
-            />
-          </Button>
+          <div className={classes.buttonwrapper}>
+            <Button
+              className={classes.Button}
+              style={{ textDecoration: "none" }}
+              href={"https://github.com/Nrin3141"}
+              target="blank"
+              rel="noopener"
+            >
+              Github <i className="fab fa-github" />
+            </Button>
+            <Button
+              className={classes.Button}
+              style={{ textDecoration: "none" }}
+              href="https://www.linkedin.com/in/ricotrebeljahr"
+              target="blank"
+              rel="noopener"
+            >
+              Linked
+              <i className="fab fa-linkedin" style={{ color: "#4875B4" }} />
+            </Button>
+          </div>
+          <div className={classes.buttonwrapper}>
+            <Button
+              className={classes.Button}
+              style={{ textDecoration: "none" }}
+              href="https://stackoverflow.com/story/ricotrebeljahr"
+              target="blank"
+              rel="noopener"
+            >
+              <i
+                className="fab fa-stack-overflow"
+                style={{ color: "#F48024" }}
+              />{" "}
+              Stackoverflow
+            </Button>
+            <Button
+              className={classes.Button}
+              style={{ textDecoration: "none" }}
+              href="https://codepen.io/Nrin/"
+              target="blank"
+              rel="noopener"
+            >
+              Codepen{" "}
+              <i
+                className="fab fa-codepen"
+                style={{ color: "rgb(100, 100, 100)" }}
+              />
+            </Button>
+          </div>
         </Toolbar>
         {children}
       </AppBar>
