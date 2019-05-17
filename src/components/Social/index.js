36de84943a20a69bbc4635d4ad3813ee7699cc01
Nrin <Nrin3141@users.noreply.github.com>
2019-05-17 22:28:11 +0200

    Add styling
    This changes the Footer's behaviour to be more responsive by making it
    flex-wrap.

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -4,7 +4,6 @@ import { withStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
-import { theme } from "../../utils/getPageContext.js"
 
 const styles = {
   appBar: {
@@ -15,13 +14,11 @@ const styles = {
     padding: "20px",
   },
   toolbar: {
-    flexDirection: "column",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
-    [theme.breakpoints.up("sm")]: {
-      flexDirection: "row",
-    },
     background: "white",
+    flexWrap: "wrap",
   },
   Button: {
     color: "white",
