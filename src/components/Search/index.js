b1ba87bda068ba83ba9026b8e227dc07b1a37b9a
Nrin <Nrin3141@users.noreply.github.com>
2019-05-19 21:19:20 +0200

    Delete blog-post images

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -14,7 +14,6 @@ const events = ["mousedown", "touchstart"]
 
 const Results = connectStateResults(
   ({ searchState: state, searchResults: res, children }) => {
-    console.log(res)
     return res && res.nbHits ? (
       children
     ) : (
