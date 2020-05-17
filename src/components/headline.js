849f9cd4f2a1273371874026f4f74d074707248e
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-17 12:52:58 +0200

    Transform css in js to plain css

--- src/components/headline.js
+++ src/components/headline.js
@@ -1,4 +1,6 @@
 import React from "react"
+import "../css/headline.css"
+
 const Headline = ({ text, secondText, parallax }) => (
   <div className={"container " + (parallax ? "absolute" : "top-margin")}>
     <div>
@@ -7,44 +9,6 @@ const Headline = ({ text, secondText, parallax }) => (
     <div className="white">
       <h1>{secondText}</h1>
     </div>
-    <style jsx>{`
-      .top-margin {
-        margin-top: 4%;
-      }
-      .white {
-        width: 100%;
-        background: white;
-      }
-      h1 {
-        color: seagreen;
-        opacity: 0.65;
-      }
-      .absolute {
-        position: absolute;
-        left: 0;
-        top: 30%;
-        margin: 0%;
-      }
-      .container {
-        flex-direction: column;
-        width: 100%;
-        color: #000;
-      }
-      div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-      }
-      .caption {
-        background-color: #111;
-        color: #fff;
-        padding: 18px;
-        font-size: 25px;
-        letter-spacing: 10px;
-      }
-    `}</style>
   </div>
 )
 export default Headline
