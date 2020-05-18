1873a4a6705fc19e39b42b9ee50dd8ffb7566ac3
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-18 12:36:32 +0200

    Clean up sections on main page

--- src/components/section.js
+++ src/components/section.js
@@ -1,94 +1,72 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Button from "@material-ui/core/Button"
 import { Link } from "gatsby"
-import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import "../css/section.css"
 
-const Section = ({ headline, href, images, counter }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allFile(
-          filter: {
-            extension: { regex: "/(jpg)|(jpeg)|(png)/" }
-            relativeDirectory: { eq: "images/banners" }
-          }
-          sort: { fields: name, order: ASC }
-        ) {
-          edges {
-            node {
-              name
-              childImageSharp {
-                fluid(maxWidth: 970) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
+const Section = ({ headline, href, img, nextSection, active }) => {
+  const speed = 200
+  const waitBeforeDelete = 2000
+  const waitBeforeChange = 700
+  const deleteSpeed = 60
+  const [timer, setTimer] = useState(0)
+  const [wait, setWait] = useState(0)
+  const [characterIndex, setCharacterIndex] = useState(0)
+
+  const count = () => {
+    setCharacterIndex(old => old + 1)
+  }
+
+  const countDown = () => {
+    setCharacterIndex(old => old - 1)
+  }
+
+  const changeToNext = () => {
+    nextSection()
+    setCharacterIndex(0)
+  }
+
+  useEffect(() => {
+    if (characterIndex >= headline.length) {
+      clearInterval(timer)
+      const startDeleting = () => setTimer(setInterval(countDown, deleteSpeed))
+      setTimeout(startDeleting, waitBeforeDelete)
+    }
+    if (characterIndex < 0) {
+      clearInterval(timer)
+      setWait(() => {
+        clearTimeout(wait)
+        return setTimeout(changeToNext, waitBeforeChange)
+      })
+    }
+  }, [characterIndex])
+
+  useEffect(() => {
+    if (active) {
+      setTimer(() => setInterval(count, speed))
+      setCharacterIndex(0)
+      return () => {
+        clearInterval(timer)
+        clearTimeout(wait)
       }
-    `}
-    render={data => {
-      return (
-        <div>
-          <div className="absolute">
-            <div>
-              <h2 style={{ marginLeft: "0.4em" }}>I am a</h2>
-              <h2 style={{ marginLeft: "0.4em" }} className="muiheadline">
-                {headline} <div className="blinking-dash" />
-              </h2>
-            </div>
-            {href !== "https://photodyssee.com" ? (
-              <Button
-                variant="contained"
-                color="secondary"
-                className="muibutton"
-              >
-                <Link
-                  style={{
-                    color: "black",
-                    textDecoration: "none",
-                  }}
-                  to={href}
-                >
-                  See for yourself!
-                </Link>
-              </Button>
-            ) : (
-              <Button
-                href={href}
-                variant="contained"
-                color="secondary"
-                className="muibutton"
-              >
-                See for yourself!
-              </Button>
-            )}
-          </div>
-          <Img
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100vw",
-              height: "100vh",
-              opacity: 0.8,
-            }}
-            fluid={
-              data.allFile.edges[
-                counter === 0 &&
-                typeof window !== "undefined" &&
-                window.innerWidth < 500
-                  ? 3
-                  : counter
-              ].node.childImageSharp.fluid
-            }
-          />
+    }
+    clearInterval(timer)
+    clearTimeout(wait)
+  }, [active])
+
+  const person =
+    headline.slice(0, characterIndex < 0 ? 0 : characterIndex) + " "
+  return (
+    <div className={active ? "activeSection" : "hiddenSection"}>
+      <Img className="section-image" fluid={img} />
+      <div className="section-container">
+        <div className="section-header">
+          <h1>I am a&nbsp;{person}</h1>
+          <span className="blinking-dash" />
         </div>
-      )
-    }}
-  />
-)
+      </div>
+    </div>
+  )
+}
 
 export default Section
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,95 @@
-import Main from "../components/main"
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Section from "../components/section"
+import MobileMenu from "../components/menus/Mobile"
+import { graphql } from "gatsby"
 import SEO from "../components/seo"
+import "../css/main.css"
+import { v4 } from "uuid"
 
-const Home = ({ classes }) => (
-  <div>
-    <SEO
-      title="Home"
-      addition="Rico's Page"
-      keywords={[`gatsby`, `application`, `react`]}
-    />
-    <Main />
-  </div>
-)
+const sections = [
+  { text: "Programmer", link: "/coding", imgPath: "programmer" },
+  { text: "Photographer", link: "/photography", imgPath: "photographer" },
+  { text: "Traveler", link: "https://photodyssee.com", imgPath: "traveler" },
+]
 
-export default Home
+const Main = ({ data }) => {
+  const [active, setActive] = useState(0)
+  const nextSection = () => {
+    setActive(old => (old + 1 >= sections.length ? 0 : old + 1))
+  }
+  return (
+    <div>
+      <SEO
+        title="Home"
+        addition="Rico's Page"
+        keywords={[`gatsby`, `application`, `react`]}
+      />
+      <div className="wrapper">
+        <div className="fixed">
+          <MobileMenu />
+        </div>
+        {sections.map((section, index) => {
+          const img = data.images.edges.find(
+            ({ node }) => node.name === section.imgPath
+          )
+          const sources = [
+            img.node.small.fluid,
+            { fluid: img.node.medium.fluid, media: "(min-width: 700px)" },
+            { fluid: img.node.full.fluid, media: "(min-width: 1800px)" },
+            { fluid: img.node.full.fluid, media: "(min-width: 3000px)" },
+          ]
+          return (
+            <Section
+              key={index}
+              active={index === active}
+              nextSection={nextSection}
+              img={sources}
+              headline={section.text}
+              href={section.link}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    images: allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+        relativeDirectory: { eq: "images/banners" }
+      }
+      sort: { fields: name, order: ASC }
+    ) {
+      edges {
+        node {
+          name
+          small: childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          medium: childImageSharp {
+            fluid(maxWidth: 1600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          large: childImageSharp {
+            fluid(maxWidth: 2400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          full: childImageSharp {
+            fluid(maxWidth: 4000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Main
