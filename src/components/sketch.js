df6825d0b41b475bdc0b7cfae9b3224cd7e620fa
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-18 16:16:30 +0200

    Try fixing sketch

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -14,8 +14,8 @@ const Dot = class Dot {
     this.target = this.p.createVector(x, y)
     this.vel = Vector.random2D()
     this.acc = this.p.createVector()
-    this.r = 5
-    this.maxspeed = 10
+    this.r = p.map(w, 0, 4000, 1, 10)
+    this.maxspeed = p.map(w, 0, 4000, 10, 20)
     this.maxforce = 1
     this.show = this.show.bind(this)
     this.applyForce = this.applyForce.bind(this)
@@ -32,6 +32,7 @@ const Dot = class Dot {
     this.acc.add(f)
   }
   flee() {
+    console.log("Flee!")
     let xDiff = this.p.mouseX - this.pos.x
     let yDiff = this.p.mouseY - this.pos.y
     let fleeVector = this.p.createVector(-xDiff, -yDiff)
@@ -61,41 +62,60 @@ const Sketch = () => {
   const sketch = p => {
     let w = myRef.current.offsetWidth
     let h = myRef.current.offsetHeight
-    let dot
     let font
-    let points
-    let points2
     let dots = []
+    let bounds1, bounds2
     const quadtree = new QuadTree(new Box(0, 0, w, h))
     p.preload = () => {
       font = p.loadFont("/Avenir.otf")
     }
     p.setup = () => {
-      p.createCanvas(w, h)
-      const l = w / 3
-      dots = [
-        ...font.textToPoints("Develop", 0, 100, l, { sampleFactor: 1 / l }),
-        ...font.textToPoints("your ideas", 0, 200, l, { sampleFactor: 1 / l }),
-      ]
-      dots.forEach(point => {
-        dot = new Dot(p, point.x, point.y, w)
-        dots.push(dot)
-      })
+      const l = w / 8
+      bounds1 = font.textBounds("Develop", 0, 0, l)
+      bounds2 = font.textBounds("your ideas", 0, -bounds1.y + bounds1.h, l)
+
+      p.createCanvas(bounds2.w, bounds1.h * 2 + 100)
+
+      console.log(bounds1)
+      console.log(bounds1)
+      const points1 = font
+        .textToPoints("Develop", 0, 0, l, {
+          sampleFactor: 30 / l,
+        })
+        .map(point => new Dot(p, point.x, point.y + bounds1.h, w))
+      const points2 = font
+        .textToPoints("your ideas", 0, -bounds1.y + 30, l, {
+          sampleFactor: 30 / l,
+        })
+        .map(point => new Dot(p, point.x, point.y + bounds2.h, w))
+      dots = [...points1, ...points2]
     }
 
     p.draw = () => {
       p.background(51)
+      p.fill("red")
+      p.circle(dots[0].pos.x, dots[0].pos.y, 30)
+      p.circle(dots[dots.length - 1].pos.x, dots[dots.length - 1].pos.y, 30)
+
       p.fill(255)
-      dots.forEach(dot => {
+      dots.forEach((dot, index) => {
         dot.arrive()
         dot.update()
         dot.show(249, 220, 92)
-        quadtree.insert(new Point(dot.pos.x, dot.pos.y, { flee: dot.flee }))
+        if (
+          (p.mouseX - dot.pos.x) * (p.mouseX - dot.pos.x) +
+            (p.mouseY - dot.pos.y) * (p.mouseY - dot.pos.y) <
+          50 * 50
+        ) {
+          dot.flee()
+        }
+        // quadtree.insert(new Point(dot.pos.x, dot.pos.y, { flee: dot.flee }))
       })
 
-      const results = quadtree.query(new Circle(p.mouseX, p.mouseY, 50))
-      results.forEach(dot => dot.data.flee())
-      quadtree.clear()
+      // const results = quadtree.query(new Circle(p.mouseX, p.mouseY, 1000))
+      // console.log(results)
+      // results.forEach(dot => dot.data.flee())
+      // quadtree.clear()
       let fps = p.frameRate()
       p.fill(255)
       p.stroke(51)
